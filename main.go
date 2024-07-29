1e0ee1230bac7e5b28a70dda60220e113227b4cc
markdimi <dimitsas.markos@gmail.com>
2024-07-29 15:51:48 +0300

    awesome commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	InitDatabase()
+	defer DB.Close()
+
+	e := gin.Default()
+	e.LoadHTMLGlob("templates/*")
+
+	e.GET("/", func(c *gin.Context) {
+		todos := ReadToDoList()
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"todos": todos,
+		})
+	})
+
+	e.POST("/todos", func(c *gin.Context) {
+		title := c.PostForm("title")
+		status := c.PostForm("status")
+		id, _ := CreateToDo(title, status)
+
+		c.HTML(http.StatusOK, "task.html", gin.H{
+			"title":  title,
+			"status": status,
+			"id":     id,
+		})
+	})
+
+	e.DELETE("/todos:id", func(c *gin.Context) {
+		param := c.Param("id")
+		id, _ := strconv.ParseInt(param, 10, 64)
+		DeleteToDo(id)
+	})
+
+	e.Run(":8000")
+
+}
---
+++ service.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ToDo struct {
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+var DB *sql.DB
+
+func InitDatabase() {
+	var err error
+	DB, err = sql.Open("sqlite3", "./awesome.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = DB.Exec(`
+	CREATE TABLE IF NOT EXISTS todos (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT,
+		status TEXT
+	);`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CreateToDo(title string, status string) (int64, error) {
+	result, err := DB.Exec("INSERT INTO todos (title, status) VALUES (?,?)", title, status)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func DeleteToDo(id int64) error {
+	_, err := DB.Exec("DELETE FROM todos WHERE id=?", id)
+	return err
+}
+
+func ReadToDoList() []ToDo {
+	rows, _ := DB.Query("SELECT id, title, status FROM todos")
+	defer rows.Close()
+
+	todos := make([]ToDo, 0)
+	for rows.Next() {
+		var todo ToDo
+		rows.Scan(&todo.Id, &todo.Title, &todo.Status)
+		todos = append(todos, todo)
+	}
+
+	return todos
+}
